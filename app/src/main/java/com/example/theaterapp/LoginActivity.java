bceadc4761f0155bf4e3dd71aaa1cc41bf22dfd7
Darling <nalgask2@gmail.com>
2023-05-06 17:41:17 +0200

    regisztráció

--- app/src/main/java/com/example/theaterapp/LoginActivity.java
+++ app/src/main/java/com/example/theaterapp/LoginActivity.java
@@ -2,14 +2,33 @@ package com.example.theaterapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.theaterapp.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
+
+    //view binding
+    private ActivityLoginBinding binding;
+
+    //firebase auth
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        //handle click, register i will go
+        binding.noAccountTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
+            }
+        });
 
 
     }
--- app/src/main/java/com/example/theaterapp/RegisterActivity.java
+++ app/src/main/java/com/example/theaterapp/RegisterActivity.java
@@ -1,14 +1,162 @@
 package com.example.theaterapp;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.example.theaterapp.databinding.ActivityRegisterBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    //view binding
+    private ActivityRegisterBinding binding;
+
+    //firebase auth
+    private FirebaseAuth firebaseAuth;
+
+    //progress dialog
+    private ProgressDialog progressDialog;
+    private String name = "", email = "", password = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        binding = ActivityRegisterBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        //init firebase auth
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        //setup progress dialog
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Kérlek várj");
+        progressDialog.setCanceledOnTouchOutside(false);
+
+        //back button for fun
+        binding.backBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+
+        //clicking and register begin for fun
+        binding.registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validateData();
+            }
+        });
+    }
+
+    private void validateData() {
+        //data validation before creating account
+        name = binding.nameEt.getText().toString().trim();
+        email = binding.emailEt.getText().toString().trim();
+        password = binding.passwordEt.getText().toString().trim();
+        String cPassword = binding.cPasswordEt.getText().toString().trim();
+
+        //validate data
+        if (TextUtils.isEmpty(name)) {
+            //if name empty
+            Toast.makeText(this, "Írd be a teljes nevedet", Toast.LENGTH_SHORT).show();
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            //if email is not entered or pattern is invalid
+            Toast.makeText(this, "Helytelen email cím", Toast.LENGTH_SHORT).show();
+        } else if (TextUtils.isEmpty(password)) {
+            //if password is empty
+            Toast.makeText(this, "Írd be a jelszavad", Toast.LENGTH_SHORT).show();
+        } else if (TextUtils.isEmpty(cPassword)) {
+            //if confirm password is empty
+            Toast.makeText(this, "Erősítse meg jelszavát", Toast.LENGTH_SHORT).show();
+        } else if (!password.equals(cPassword)) {
+            //if 2 password doesn't match
+            Toast.makeText(this, "A két jelszó nem egyezik", Toast.LENGTH_SHORT).show();
+        } else {
+            //if everything is good, the life is good, create account
+            createUserAccount();
+        }
+    }
+
+    private void createUserAccount() {
+        //show progress
+        progressDialog.setMessage("Felhasználó létrehozása");
+        progressDialog.show();
+
+        //create user in firebase auth
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        //Account is gut, add the account I will
+                        //progressDialog.dismiss();
+                        updateUserInfo();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(Exception e) {
+                        //Account is no gut
+                        progressDialog.dismiss();
+                        Toast.makeText(RegisterActivity.this, "" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
+    }
+
+    private void updateUserInfo() {
+        progressDialog.setMessage("Felhasználó elmentése");
+
+        //timestamp
+        long timestamp = System.currentTimeMillis();
+
+        //get user uid
+        String uid = firebaseAuth.getUid();
+
+        //setup data to add in db
+        HashMap<String, Object> hashMap = new HashMap<>();
+        hashMap.put("uid", uid);
+        hashMap.put("email", email);
+        hashMap.put("name", name);
+        hashMap.put("profileImage", " ");
+        hashMap.put("userType", "user");
+        hashMap.put("timestamp", timestamp);
+
+        //set data to db
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Users");
+        ref.child(uid)
+                .setValue(hashMap)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        //add
+                        progressDialog.dismiss();
+                        Toast.makeText(RegisterActivity.this, "Sikeres regisztráció", Toast.LENGTH_SHORT).show();
+                        startActivity(new Intent(RegisterActivity.this, UserActivity.class));
+                        finish();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(Exception e) {
+                        //failed
+                        Toast.makeText(RegisterActivity.this, "" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
