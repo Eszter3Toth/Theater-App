de62118b7bc4198d8ea7ea5e15e7b4e1324437d9
Darling <nalgask2@gmail.com>
2023-05-06 20:49:42 +0200

    login

--- app/src/main/java/com/example/theaterapp/LoginActivity.java
+++ app/src/main/java/com/example/theaterapp/LoginActivity.java
@@ -1,13 +1,27 @@
 package com.example.theaterapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.theaterapp.databinding.ActivityLoginBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -15,12 +29,25 @@ public class LoginActivity extends AppCompatActivity {
     private ActivityLoginBinding binding;
 
     //firebase auth
+    private FirebaseAuth firebaseAuth;
+
+    //progress dialog
+    private ProgressDialog progressDialog;
+    private String email = "", password = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        //init firebase auth
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        //setup progress dialog
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Kérlek várj");
+        progressDialog.setCanceledOnTouchOutside(false);
 
         //handle click, register i will go
         binding.noAccountTv.setOnClickListener(new View.OnClickListener() {
@@ -30,6 +57,91 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        //handle click, login i will
+        binding.loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validateData();
+            }
+        });
+
+
+    }
+
+    private void validateData() {
+        //get data
+        email = binding.emailEt.getText().toString().trim();
+        password = binding.passwordEt.getText().toString().trim();
+
+        //validate data
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            //if email is not entered or pattern is invalid
+            Toast.makeText(this, "Helytelen email cím", Toast.LENGTH_SHORT).show();
+        } else if (TextUtils.isEmpty(password)) {
+            //if password is empty
+            Toast.makeText(this, "Írd be a jelszavad", Toast.LENGTH_SHORT).show();
+        } else {
+            loginUser();
+        }
+    }
+
+    private void loginUser() {
+        //progress so people will not be frustrated
+        progressDialog.setMessage("Bejelentkezés");
+        progressDialog.show();
+
+        //login the user cuz we are kind
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        checkUser();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        progressDialog.dismiss();
+                        Toast.makeText(LoginActivity.this, "" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+    }
+
+    private void checkUser() {
+        progressDialog.setMessage("Felhasználó ellenőrzése");
+        //check if you are admin or the normal one
+        //get current user
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+
+        //check in db
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Users");
+        ref.child(firebaseUser.getUid())
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        progressDialog.dismiss();
+                        //get user type
+                        String userType = "" + snapshot.child("userType").getValue();
+                        //chech user type
+                        if (userType.equals("user")) {
+                            startActivity(new Intent(LoginActivity.this, UserActivity.class));
+                            finish();
+                        } else {
+                            startActivity(new Intent(LoginActivity.this, AdminActivity.class));
+                            finish();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
 
     }
 }
+
+
--- app/src/main/java/com/example/theaterapp/SplashActivity.java
+++ app/src/main/java/com/example/theaterapp/SplashActivity.java
@@ -1,25 +1,76 @@
 package com.example.theaterapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class SplashActivity extends AppCompatActivity {
+
+    //firebase auth
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        //init firebase auth
+        firebaseAuth = FirebaseAuth.getInstance();
+
         //start the main after 1 second
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(SplashActivity.this, MainActivity.class));
-                finish();
+                checkUser();
             }
         }, 2000);
+    }
+
+    private void checkUser() {
+        //get current user
+        FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
+        if (firebaseUser == null){
+            //user is not logged in
+            startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            finish();
+        }
+        else {
+            DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Users");
+            ref.child(firebaseUser.getUid())
+                    .addListenerForSingleValueEvent(new ValueEventListener() {
+
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            //get user type
+                            String userType = "" + snapshot.child("userType").getValue();
+                            //chech user type
+                            if (userType.equals("user")) {
+                                startActivity(new Intent(SplashActivity.this, UserActivity.class));
+                                finish();
+                            } else {
+                                startActivity(new Intent(SplashActivity.this, AdminActivity.class));
+                                finish();
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+        }
+
+
     }
 }
